56e5916ad40c8a5f73b4cddd6d94788def29b488
anaalta <suciuanamaria@yahoo.com>
2017-04-18 16:06:58 +0200

    added a balance 0 functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
-decribe Oystercard do
+require 'oystercard'
+describe Oystercard do
+
+it 'checks if a new card has a balance of 0' do
+  card = Oystercard.new
+  expect(card.balance).to eq 0
+end
 
 end
