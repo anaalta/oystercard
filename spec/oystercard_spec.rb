533e1704f5a3afc6eb244b38e249ed67f8a8bf19
anaalta <suciuanamaria@yahoo.com>
2017-04-18 16:47:57 +0200

    top-uo feature added, challange 5 done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,17 @@
 require 'oystercard'
 describe Oystercard do
 
+
 it 'checks if a new card has a balance of 0' do
   card = Oystercard.new
   expect(card.balance).to eq 0
+end
+
+describe '#top_up' do
+  it 'tops up a card' do
+    expect(subject.top_up(3)).to eq (3)
+    expect {subject.top_up 1}.to change {subject.balance}.by 1
+  end
 end
 
 end
